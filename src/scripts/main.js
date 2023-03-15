d7df121eeef5907a057b46de3c5e3443e774f276
Reinen <reinenconstantino@gmail.com>
2023-03-15 19:59:15 +0800

    Added Contact Us, Article and  Ekis Sumbungan Page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -26,3 +26,50 @@ const swiper = new Swiper('.swiper-container', {
     clickable: true,
   },
 });
+
+// * POPOVER
+const popoverTriggers = document.querySelectorAll('[data-popover-target]');
+popoverTriggers.forEach((trigger) => {
+  const popoverName = trigger.getAttribute('data-popover-target');
+  const popover = document.querySelector(
+    `[data-popover-name="${popoverName}"]`,
+  );
+  if (popover) {
+    const closeBtn = popover.querySelector('[data-close-popover]');
+    if (closeBtn) {
+      closeBtn.addEventListener('click', () => {
+        popover.classList.remove('opacity-100');
+        popover.classList.add('opacity-0');
+        setTimeout(() => {
+          popover.classList.add('hidden');
+        }, 10);
+      });
+    }
+    trigger.addEventListener('click', () => {
+      popover.classList.remove('hidden');
+      setTimeout(() => {
+        popover.classList.remove('opacity-0');
+        popover.classList.add('opacity-100');
+      }, 10);
+    });
+  }
+});
+
+// * Form Wizard
+const form = document.querySelector('form');
+const nextBtn = document.querySelector('.btn-next');
+
+nextBtn.addEventListener('click', (event) => {
+  console.log('asdas');
+  event.preventDefault();
+  const inputs = form.querySelectorAll('input');
+  const valid = Array.from(inputs).every((input) => input.value);
+  if (valid) {
+    const currentStep = parseInt(form.dataset.step);
+    const nextStep = currentStep + 1;
+    const nextSection = document.querySelector(`[data-step="${nextStep}"]`);
+    form.dataset.step = nextStep;
+    form.classList.add('hidden');
+    nextSection.classList.remove('hidden');
+  }
+});
