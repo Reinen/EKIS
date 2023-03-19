dfd9f297a0bbdc035c41fef03ac7da0a7b45f102
Reinen <reinenconstantino@gmail.com>
2023-03-19 16:14:50 +0800

    Multisteps form and  Overlay on Hotspots page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -31,6 +31,7 @@ const swiper = new Swiper('.swiper-container', {
 
 // * POPOVER
 const popoverTriggers = document.querySelectorAll('[data-popover-target]');
+const bodyOverflow = document.querySelector('body');
 popoverTriggers.forEach((trigger) => {
   const popoverName = trigger.getAttribute('data-popover-target');
   const popover = document.querySelector(
@@ -44,11 +45,13 @@ popoverTriggers.forEach((trigger) => {
         popover.classList.add('opacity-0');
         setTimeout(() => {
           popover.classList.add('hidden');
+          bodyOverflow.style.overflow = 'auto';
         }, 10);
       });
     }
     trigger.addEventListener('click', () => {
       popover.classList.remove('hidden');
+      bodyOverflow.style.overflow = 'hidden';
       setTimeout(() => {
         popover.classList.remove('opacity-0');
         popover.classList.add('opacity-100');
@@ -56,3 +59,121 @@ popoverTriggers.forEach((trigger) => {
     });
   }
 });
+
+// * DRAG AND DROP IMAGE UPLOAD WITH PREVIEW
+const dropZone = document.querySelector('.border-dashed');
+const imagePreviewContainer = document.querySelector(
+  '#image-preview-container',
+);
+
+dropZone.addEventListener('dragover', (e) => {
+  e.preventDefault();
+  dropZone.classList.add('border-blue-500');
+});
+
+dropZone.addEventListener('dragleave', () => {
+  dropZone.classList.remove('border-blue-500');
+});
+
+dropZone.addEventListener('drop', (e) => {
+  e.preventDefault();
+  dropZone.classList.remove('border-blue-500');
+  const files = e.dataTransfer.files;
+  handleFiles(files);
+});
+
+const fileInput = document.querySelector('#image-upload');
+fileInput.addEventListener('change', () => {
+  const files = fileInput.files;
+  handleFiles(files);
+});
+
+function handleFiles(files) {
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    const reader = new FileReader();
+    reader.onload = () => {
+      const imagePreview = document.createElement('div');
+      imagePreview.classList.add('relative', 'rounded-lg', 'shadow-md', 'm-2');
+      const deleteButton = document.createElement('button');
+      deleteButton.innerHTML = '&times;';
+      deleteButton.classList.add(
+        'absolute',
+        'top-0',
+        'right-0',
+        'text-red-500',
+        'cursor-pointer',
+        'hover:text-red-600',
+        'bg-white',
+        'font-bold',
+        'px-2',
+      );
+      deleteButton.addEventListener('click', () => {
+        imagePreview.remove();
+      });
+      const img = document.createElement('img');
+      img.src = reader.result;
+      img.classList.add('w-full', 'rounded-lg');
+      imagePreview.appendChild(deleteButton);
+      imagePreview.appendChild(img);
+      imagePreviewContainer.appendChild(imagePreview);
+    };
+    reader.readAsDataURL(file);
+  }
+}
+
+// * FORM WIZARD
+const form = document.querySelector('#form-wizard');
+const prevBtn = document.querySelectorAll('.prev-btn');
+const nextBtn = document.querySelectorAll('.next-btn');
+
+let currentStep = 0;
+
+function showStep(step) {
+  scrollToFormTop(-50);
+  const formSteps = form.querySelectorAll('[data-form]');
+  formSteps.forEach((formStep, index) => {
+    if (index === step) {
+      formStep.classList.remove('hidden');
+    } else {
+      formStep.classList.add('hidden');
+    }
+  });
+
+  const currentSteps = document.querySelectorAll('[data-step]');
+  currentSteps.forEach((currentStep, index) => {
+    if (index === step) {
+      currentStep.classList.add('active');
+    } else {
+      currentStep.classList.remove('active');
+    }
+  });
+}
+
+function scrollToFormTop(offset) {
+  const form = document.querySelector('body');
+  const formTop = form.getBoundingClientRect().top;
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  const offsetTop = formTop + scrollTop - offset;
+  window.scrollTo({ top: offsetTop, behavior: 'smooth' });
+}
+
+function handlePrev() {
+  currentStep--;
+  showStep(currentStep);
+}
+
+function handleNext() {
+  currentStep++;
+  showStep(currentStep);
+}
+
+prevBtn.forEach((button) => {
+  button.addEventListener('click', handlePrev);
+});
+
+nextBtn.forEach((button) => {
+  button.addEventListener('click', handleNext);
+});
+
+showStep(currentStep);
